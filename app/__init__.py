fee1d0ba3eabfc2ca90bfc013883db5a3619c5df
Rafael Lima <rafaeldblima@gmail.com>
2020-05-05 21:27:03 -0400

    Adding auth module

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
 from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
@@ -10,6 +11,8 @@ bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
 
 
 def create_app(config_name):
@@ -21,8 +24,14 @@ def create_app(config_name):
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
+    # Main module
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    # Auth module
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
--- app/main/views.py
+++ app/main/views.py
@@ -1,36 +1,11 @@
-from datetime import datetime
-
-from flask import render_template, session, redirect, url_for, make_response, abort, flash, current_app
+from flask import render_template, url_for, make_response, abort
 
 from . import main
-from .forms import NameForm
-from .. import db
-from ..email import send_email
-from ..models import User
 
 
-@main.route("/", methods=['GET', 'POST'])
+@main.route("/")
 def index():
-    # from flask import request
-    # user_agent = request.headers.get('User-Agent')
-    # return '<p>Your browser is {}</p>'.format(user_agent)
-    form = NameForm()
-    if form.validate_on_submit():
-        db_user = User.query.filter_by(username=form.name.data).first()
-        if db_user is None:
-            flash('User created!')
-            db_user = User(username=form.name.data)
-            db.session.add(db_user)
-            db.session.commit()
-            session['known'] = False
-            if current_app.config['FLASK_ADMIN']:
-                send_email(current_app.config['FLASK_ADMIN'], 'New User', 'mail/new_user', user=db_user)
-        else:
-            flash('Welcome Back!')
-            session['known'] = True
-        return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get("name"),
-                           current_time=datetime.utcnow(), known=session.get('known', False))
+    return render_template('index.html')
 
 
 @main.route("/user/<name>")
